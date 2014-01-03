f792a0aee4839bce969b715670df803f0c529cd3
Hiromi Ishii <konn.jinro@gmail.com>
2014-01-03 17:04:39 +0900

    setuped ruby

--- site-start.d/init_regex.el
+++ site-start.d/init_regex.el
@@ -3,4 +3,9 @@
        '(foreign-regexp/regexp-type 'ruby)
        '(reb-re-syntax 'foreign-regexp))))
 
+(global-set-key "\M-\C-s" 'foreign-regexp/isearch-forward)
+(global-set-key "\M-\C-r" 'foreign-regexp/isearch-backward)
+(global-set-key (kbd "M-C-;") 'foreign-regexp/query-replace)
+
+
 (provide 'init_regex)
