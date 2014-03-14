55f5458abcd903db7216cc0bbc68be575bcf0ec6
Hiromi Ishii <konn.jinro@gmail.com>
2014-03-14 14:54:20 +0900

    changed auto-complete settings

--- site-start.d/init_auto-complete.el
+++ site-start.d/init_auto-complete.el
@@ -1,5 +1,16 @@
-(require 'auto-complete)
-(global-auto-complete-mode t)
+(require 'auto-complete-config)
+(ac-config-default)
+(add-to-list 'ac-dictionary-directories "~/.emacs.d/share/ac-dict")
+; (global-auto-complete-mode t)
 
+(setq-default ac-sources (append '(ac-source-yasnippet
+                ac-source-abbrev
+                ac-source-dictionary
+                ac-source-words-in-same-mode-buffers) ac-sources))
+
+; (add-to-list 'ac-sources 'ac-source-yasnippet)
+;  
+; (setf (symbol-function 'yas-active-keys)
+;       (lambda ()
+;         (remove-duplicates (mapcan #'yas--table-all-keys (yas--get-snippet-tables)))))
 (provide 'init_auto-complete)
-
