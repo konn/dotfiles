35cee44f183d879ec525f09d1d8d5bff359ef497
Hiromi Ishii <konn.jinro@gmail.com>
2015-10-17 22:59:45 +0900

    Switched from foreign-regexp to visual-regexp-steroids

--- site-start.d/init_regex.el
+++ site-start.d/init_regex.el
@@ -1,11 +1,22 @@
-(cond ((require 'foreign-regexp nil t)
-       (custom-set-variables
-       '(foreign-regexp/regexp-type 'ruby)
-       '(reb-re-syntax 'foreign-regexp))))
+(require 'visual-regexp-steroids)
+(setq vr/engine 'python)                ;python regexpならばこれ
+;; (setq vr/engine 'pcre2el)               ;elispでPCREから変換
+(global-set-key (kbd "M-%") 'vr/query-replace)
+;; multiple-cursorsを使っているならこれで
+(global-set-key (kbd "C-c m") 'vr/mc-mark)
+;; 普段の正規表現isearchにも使いたいならこれを
+(global-set-key (kbd "C-M-r") 'vr/isearch-backward)
+(global-set-key (kbd "C-M-s") 'vr/isearch-forward)
 
-(global-set-key "\M-\C-s" 'foreign-regexp/isearch-forward)
-(global-set-key "\M-\C-r" 'foreign-regexp/isearch-backward)
-(global-set-key (kbd "M-C-;") 'foreign-regexp/query-replace)
+
+; (cond ((require 'foreign-regexp nil t)
+;        (custom-set-variables
+;        '(foreign-regexp/regexp-type 'ruby)
+;        '(reb-re-syntax 'foreign-regexp))))
+;  
+; (global-set-key "\M-\C-s" 'foreign-regexp/isearch-forward)
+; (global-set-key "\M-\C-r" 'foreign-regexp/isearch-backward)
+; (global-set-key (kbd "M-C-;") 'foreign-regexp/query-replace)
 
 
 (provide 'init_regex)
