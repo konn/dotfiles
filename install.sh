601760bf95eb34758c395709b8c24bbc6835763b
Hiromi ISHII <konn.jinro@gmail.com>
2018-01-30 01:35:42 +0900

    Change the way to sync with .zsh.d so that it overrides all ~/.zsh.d as single symlink to automatically track new setting files.
    * Ideally, .emacs.d should also be treated in such a way, but we can't do entirely.
      This is because there is a local file that cannot be published publicly.
      Perhaps `.gitignore`-ing such directories makes sence, but we need more organised installer script which can be configurable.

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env zsh
 
 # Overlaying existing conf directories
-overlays=(.emacs.d .stack .cabal .zsh.d)
+overlays=(.emacs.d .stack .cabal)
 for dir_name in ${overlays}; do
   TARGET=$HOME/${dir_name}
   MINE=$(pwd)/${dir_name}
@@ -10,11 +10,6 @@ for dir_name in ${overlays}; do
       targ=$(echo "${i}" | awk -F"${MINE}" '{print $NF}')
       if [ -f "${i}" ] ; then
          mkdir -p "${TARGET}$(dirname ${targ})"
-         if [ -f "${TARGET}${targ}" ]; then
-             echo "The file ${TARGET}${targ} already exists."
-             echo "Saved as: ${TARGET}${targ}.orig"
-             mv ${TARGET}${targ} ${TARGET}${targ}.orig
-         fi
          ln -sf ${MINE}${targ} ${TARGET}${targ}
       fi
   done
@@ -23,7 +18,8 @@ done
 # Single dot files
 dots=(.ghci .gitattributes_global .gitconfig .gitignore_global)
 dots=($dots .inputrc .irbrc .languagetool.cfg .latexmkrc)
-dots=($dots .nethackrc .profile .stylish-haskell.yaml .zshrc)
+dots=($dots .nethackrc .profile .stylish-haskell.yaml)
+dots=($dots .zsh.d .zshrc)
 for dot in ${dots}; do
   ln -sf "$(pwd)/${dot}" "${HOME}/${dot}"
 done
