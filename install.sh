40f645e1cdbc857ec218a06f714b774c26c4b6ff
Hiromi ISHII <konn.jinro@gmail.com>
2018-01-29 23:41:12 +0900

    Install script for .emacs.d

--- install.sh
+++ install.sh
@@ -5,3 +5,14 @@ ln -sf $(pwd)/.zshrc.d                $HOME/.zshrc.d
 mkdir -p $HOME/.cabal
 ln -sf $(pwd)/.cabal/config           $HOME/.cabal
 ln -sf $(pwd)/.cabal/config.platform  $HOME/.cabal
+
+EMACS_HOME=$HOME/.emacs.d
+MY_EMACS=$(pwd)/.emacs.d
+mkdir -p ${EMACS_HOME}
+for i in ${MY_EMACS}/**/*; do
+    targ=$(echo "${i}" | awk -F"${MY_EMACS}" '{print $NF}')
+    if [ -f "${i}" ] ; then
+       mkdir -p "${EMACS_HOME}$(dirname ${targ})"
+       ln -sf ${MY_EMACS}${targ} ${EMACS_HOME}${targ}
+    fi
+done
