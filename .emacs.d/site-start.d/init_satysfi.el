6df551b687df9bcdaa3da092b7e9c84abb021a69
Hiromi ISHII <konn.jinro@gmail.com>
2018-03-03 01:52:44 +0900

    Flycheck satysfi

--- .emacs.d/site-start.d/init_satysfi.el
+++ .emacs.d/site-start.d/init_satysfi.el
@@ -1,9 +1,19 @@
 (require 'satysfi)
+(require 'flycheck)
 (setq auto-mode-alist (cons '("\\.saty$" . satysfi-mode) auto-mode-alist))
 (setq auto-mode-alist (cons '("\\.satyh$" . satysfi-mode) auto-mode-alist))
 (eval-after-load "satysfi"
   '(progn
      (define-key satysfi-mode-map (kbd "C-z") nil)
      )
+  )
+(flycheck-define-checker satysfi
+  "A SATySFi syntax and type checker using SATySFi compiler"
+
+  :command ("satysfi" source)
+  :error-patterns
+   ((error
+     bol "![" (message) "] at line" (zero-or-more space) line ", characters" (zero-or-more space) column "-" (zero-or-more anything) eol))
+  :modes satysfi-mode
   )
 (provide 'init_satysfi)
