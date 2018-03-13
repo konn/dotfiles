e4c2163f86720e064f8e72487f877e6a37a325e4
Hiromi ISHII <konn.jinro@gmail.com>
2018-03-13 16:49:49 +0900

    Adding more and more safe vars

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -211,7 +211,8 @@
  '(rw-hunspell-use-rw-ispell t)
  '(safe-local-variable-values
    (quote
-    ((intero-targets "halg-algorithms:lib")
+    ((intero-targets "halg-polyn-parser:lib")
+     (intero-targets "halg-algorithms:lib")
      (intero-targets "halg-polynomials:lib")
      (TeX-master . "saturated-filters.tex")
      (intero-targets "ghc-typelits-presburger:exe:simple-arith")
