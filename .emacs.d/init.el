63fb3cb52820bf695b045ac9bad19b392155c148
Hiromi ISHII <konn.jinro@gmail.com>
2018-03-13 21:01:33 +0900

    Anzen varyuu

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -211,7 +211,8 @@
  '(rw-hunspell-use-rw-ispell t)
  '(safe-local-variable-values
    (quote
-    ((intero-targets "halg-bridge-singular:lib")
+    ((intero-targets "halg-core:lib")
+     (intero-targets "halg-bridge-singular:lib")
      (intero-targets "halg-polyn-parser:lib")
      (intero-targets "halg-algorithms:lib")
      (intero-targets "halg-polynomials:lib")
