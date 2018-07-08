0f938182994077057c93bf9ff4405ce1c1d173d5
Hiromi ISHII <konn.jinro@gmail.com>
2018-07-08 15:31:11 +0100

    yacctt

--- .emacs.d/site-start.d/init_cubical.el
+++ .emacs.d/site-start.d/init_cubical.el
@@ -1,4 +1,7 @@
 (autoload 'cubicaltt-mode "cubicaltt" "cubical editing mode" t)
 (setq auto-mode-alist (append auto-mode-alist '(("\\.ctt$" . cubicaltt-mode))))
 
+(autoload 'yacctt-mode "yacctt" "cubical editing mode" t)
+(setq auto-mode-alist (append auto-mode-alist '(("\\.ytt$" . yacctt-mode))))
+
 (provide 'init_cubical)
