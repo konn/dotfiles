f8194e41e973d6f5c6996035c05919e94f9eb9b9
Hiromi ISHII <konn.jinro@gmail.com>
2018-08-16 16:14:26 +0900

    requires

--- .emacs.d/site-start.d/init_rust.el
+++ .emacs.d/site-start.d/init_rust.el
@@ -1,3 +1,5 @@
+(require 'rust-mode)
+(require 'racer)
 (add-to-list 'exec-path (expand-file-name "~/.cargo/bin/"))
 ;;; rust-modeでrust-format-on-saveをtにすると自動でrustfmtが走る
 (eval-after-load "rust-mode"
