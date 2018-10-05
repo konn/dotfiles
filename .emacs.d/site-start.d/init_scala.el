e287f3a1ac03419d5ddaf54ab4b903469169a184
Hiromi ISHII <konn.jinro@gmail.com>
2018-10-05 23:44:46 +0900

    Sealed abstract class

--- .emacs.d/site-start.d/init_scala.el
+++ .emacs.d/site-start.d/init_scala.el
@@ -1,7 +1,7 @@
 (require 'scala-mode-auto)
 (eval-after-load 'scala-mode
   '(progn
-     (define-key company-active-map [tab] nil)
+     (define-key scala-mode-map [tab] nil)
      (define-key scala-mode-map [tab] 'yas-expand)
      (define-key scala-mode-map (kbd "C-:") 'company-complete-common)
      ))
