066857f0fac46a818fce2f55d0c0ff949c23cd82
Hiromi ISHII <konn.jinro@gmail.com>
2018-10-05 21:24:44 +0900

    Scala Keybindings

--- .emacs.d/site-start.d/init_scala.el
+++ .emacs.d/site-start.d/init_scala.el
@@ -1,7 +1,9 @@
 (require 'scala-mode-auto)
 (eval-after-load 'scala-mode
   '(progn
-     (define-key scala-mode-map (kbd "C-.") 'yas-expand)
+     (define-key company-active-map [tab] nil)
+     (define-key scala-mode-map [tab] 'yas-expand)
+     (define-key scala-mode-map (kbd "C-:") 'ensime-company-complete-or-indent)
      ))
 
 (provide 'init_scala)
