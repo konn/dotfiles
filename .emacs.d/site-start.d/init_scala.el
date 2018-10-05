56c3b3eb5c1e9fec8280bbe929d46404d660fefc
Hiromi ISHII <konn.jinro@gmail.com>
2018-10-05 23:59:49 +0900

    C-. for yas

--- .emacs.d/site-start.d/init_scala.el
+++ .emacs.d/site-start.d/init_scala.el
@@ -3,6 +3,7 @@
   '(progn
      (define-key scala-mode-map [tab] nil)
      (define-key scala-mode-map [tab] 'yas-expand)
+     (define-key scala-mode-map (kbd "C-.") 'yas-expand)
      (define-key scala-mode-map (kbd "C-:") 'company-complete-common)
      ))
 
