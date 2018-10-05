f839149573805297df2a232c0d422ee67b240816
Hiromi ISHII <konn.jinro@gmail.com>
2018-10-05 22:54:36 +0900

    company

--- .emacs.d/site-start.d/init_scala.el
+++ .emacs.d/site-start.d/init_scala.el
@@ -3,7 +3,7 @@
   '(progn
      (define-key company-active-map [tab] nil)
      (define-key scala-mode-map [tab] 'yas-expand)
-     (define-key scala-mode-map (kbd "C-:") 'ensime-company-complete-or-indent)
+     (define-key scala-mode-map (kbd "C-:") 'company-complete-common)
      ))
 
 (provide 'init_scala)
