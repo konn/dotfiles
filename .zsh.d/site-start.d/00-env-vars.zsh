ededd3fede08d12720450cf4c81fc3bfe8ec8335
Hiromi ISHII <konn.jinro@gmail.com>
2020-01-01 21:31:12 +0900

    openssl paths

--- .zsh.d/site-start.d/00-env-vars.zsh
+++ .zsh.d/site-start.d/00-env-vars.zsh
@@ -53,3 +53,7 @@ else
   export EDITOR=/usr/bin/emacs
   export ALTERNATE_EDITOR=/usr/bin/emacs
 fi
+export PATH="/usr/local/opt/openssl@1.1/bin:$PATH"
+export LDFLAGS="-L/usr/local/opt/openssl@1.1/lib"
+export CPPFLAGS="-I/usr/local/opt/openssl@1.1/include"
+export PKG_CONFIG_PATH="/usr/local/opt/openssl@1.1/lib/pkgconfig"
